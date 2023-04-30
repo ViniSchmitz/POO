312ff3ca28368c602c71eef88c53fc3356e7525b
ViniSchmitz <106819330+ViniSchmitz@users.noreply.github.com>
2023-04-29 23:06:21 -0300

    Exercício 2
    exercicio 2

---
+++ FolhaDePagamento.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class FolhaDePagamento{
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("informe um numero inteiro: ");
+        int b = teclado.nextInt();
+        System.out.print("informe um numero inteiro: ");
+        int c = teclado.nextInt();
+        float a = b*c;
+        System.out.println(a);
+        System.out.println("O programa executa um calculo de exemplo de folha de pagamento.");
+
+
+    }    
+}
